09b04322fd47ddc2ff27d60351c39adde321f6b7
Vinicius Rodrigues <rodrigues.vini.2004@gmail.com>
2025-04-10 18:48:01 -0300

    :sparkles: feat: Adicionado getById em PostController

--- src/main/java/com/personalblog/personalblog/controller/PostController.java
+++ src/main/java/com/personalblog/personalblog/controller/PostController.java
@@ -3,13 +3,12 @@ package com.personalblog.personalblog.controller;
 import com.personalblog.personalblog.model.Post;
 import com.personalblog.personalblog.repository.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/posts")
@@ -22,6 +21,13 @@ public class PostController {
     @GetMapping
     public ResponseEntity<List<Post>> getAll() {
         return ResponseEntity.ok(postRepository.findAll());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Post> getById(@PathVariable long id) {
+        return postRepository.findById(id)
+                .map(response -> ResponseEntity.ok(response))
+                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
     }
 
 }
