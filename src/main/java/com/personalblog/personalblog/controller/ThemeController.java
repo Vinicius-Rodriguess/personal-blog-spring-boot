276b56de9a714e178a4738fb9dfc0594da71d053
Vinicius Rodrigues <rodrigues.vini.2004@gmail.com>
2025-04-28 16:20:12 -0300

    :sparkles: feat: Adicionado createTheme em ThemeController

--- src/main/java/com/personalblog/personalblog/controller/ThemeController.java
+++ src/main/java/com/personalblog/personalblog/controller/ThemeController.java
@@ -2,6 +2,7 @@ package com.personalblog.personalblog.controller;
 
 import com.personalblog.personalblog.model.Theme;
 import com.personalblog.personalblog.repository.ThemeRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -33,5 +34,11 @@ public class ThemeController {
     public ResponseEntity<List<Theme>> getByDescription(@PathVariable String description){
         return ResponseEntity.ok(themeRepository
                 .findAllByDescriptionContainingIgnoreCase(description));
+    }
+
+    @PostMapping
+    public ResponseEntity<Theme> createTheme(@Valid @RequestBody Theme theme){
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(themeRepository.save(theme));
     }
 }
