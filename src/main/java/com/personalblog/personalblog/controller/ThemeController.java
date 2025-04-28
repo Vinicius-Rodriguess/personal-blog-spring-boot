4d8d042071d160f586a089022c26594212a4cb9e
Vinicius Rodrigues <rodrigues.vini.2004@gmail.com>
2025-04-28 16:16:19 -0300

    :sparkles: feat: Adicionado getById em ThemeController

--- src/main/java/com/personalblog/personalblog/controller/ThemeController.java
+++ src/main/java/com/personalblog/personalblog/controller/ThemeController.java
@@ -3,11 +3,9 @@ package com.personalblog.personalblog.controller;
 import com.personalblog.personalblog.model.Theme;
 import com.personalblog.personalblog.repository.ThemeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,4 +22,10 @@ public class ThemeController {
         return ResponseEntity.ok(themeRepository.findAll());
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Theme> getById(@PathVariable Long id){
+        return themeRepository.findById(id)
+                .map(response -> ResponseEntity.ok(response))
+                .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
 }
