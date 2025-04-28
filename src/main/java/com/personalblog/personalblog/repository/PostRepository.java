fb4a82d7f944cfd02de0f2771d1a1b7c2c6d51f9
Vinicius Rodrigues <rodrigues.vini.2004@gmail.com>
2025-04-28 09:24:51 -0300

    :sparkles: feat: Adicionado getByTitle em PostController

--- src/main/java/com/personalblog/personalblog/repository/PostRepository.java
+++ src/main/java/com/personalblog/personalblog/repository/PostRepository.java
@@ -2,7 +2,10 @@ package com.personalblog.personalblog.repository;
 
 import com.personalblog.personalblog.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
-
+    public List <Post> findAllByTitleContainingIgnoreCase(@Param("title") String title);
 }
