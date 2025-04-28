63294be8a93a38c5c70d13e407a9472cc08af3b7
Vinicius Rodrigues <rodrigues.vini.2004@gmail.com>
2025-04-28 15:50:07 -0300

    :sparkles: feat: Adicionado entidade Theme

---
+++ src/main/java/com/personalblog/personalblog/model/Theme.java
@@ -0,0 +1,37 @@
+package com.generation.blogpessoal.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "tb_theme")
+public class Theme {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "\n" +
+            "The Description Attribute is mandatory")
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
