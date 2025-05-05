bb76f05cff685179bc4068d099c3edc71cbe0736
Vinicius-Rodriguess <rodrigues.vini.2004@gmail.com>
2025-05-05 18:50:29 -0300

    :sparkles: feat: Adicionado Entidade User

---
+++ src/main/java/com/personalblog/personalblog/model/User.java
@@ -0,0 +1,93 @@
+package com.personalblog.personalblog.model;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "The Name attribute is required!")
+    private String name;
+
+    @NotNull(message = "The Email attribute is required!")
+    @Email(message = "The Email attribute must be a valid email address!")
+    private String email;
+
+    @NotBlank(message = "The Password attribute is required!")
+    @Size(min = 8, message = "The Password must be at least 8 characters long")
+    private String password;
+
+    @Size(max = 5000, message = "The photo link cannot exceed 5000 characters")
+    private String photo;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "user", cascade = CascadeType.REMOVE)
+    @JsonIgnoreProperties("user")
+    private List<Post> posts;
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhoto() {
+        return this.photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public List<Post> getPosts() {
+        return this.posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
+    }
+}
