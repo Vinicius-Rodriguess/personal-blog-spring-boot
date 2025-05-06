c28559dace9900797bb508fff9271b182615f313
Vinicius-Rodriguess <rodrigues.vini.2004@gmail.com>
2025-05-06 10:42:57 -0300

    :sparkles: feat: Adicionado UserService

---
+++ src/main/java/com/personalblog/personalblog/service/UserService.java
@@ -0,0 +1,109 @@
+package com.personalblog.personalblog.service;
+
+import java.util.Optional;
+
+import com.personalblog.personalblog.model.User;
+import com.personalblog.personalblog.model.UserLogin;
+import com.personalblog.personalblog.repository.UserRepository;
+import com.personalblog.personalblog.security.JwtService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    public Optional<User> createUser(User user) {
+
+        if (userRepository.findByEmail(user.getEmail()).isPresent())
+            return Optional.empty();
+
+        user.setPassword(encryptPassword(user.getPassword()));
+
+        return Optional.of(userRepository.save(user));
+
+    }
+
+    public Optional<User> updateUser(User user) {
+
+        if(userRepository.findById(user.getId()).isPresent()) {
+
+            Optional<User> searchUser = userRepository.findByEmail(user.getEmail());
+
+            if ( searchUser.isPresent() && !searchUser.get().getId().equals(user.getId()))
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "User already exists!", null);
+
+            user.setPassword(encryptPassword(user.getPassword()));
+
+            return Optional.ofNullable(userRepository.save(user));
+
+        }
+
+        return Optional.empty();
+
+    }
+
+    public Optional<UserLogin> authenticateUser(Optional<UserLogin> userLogin) {
+
+        if (userLogin.isEmpty()) return Optional.empty();
+
+        // Gera o Objeto de autenticação
+        var credentials = new UsernamePasswordAuthenticationToken(userLogin.get().getEmail(), userLogin.get().getPassword());
+
+        // Autentica o Usuario
+        Authentication authentication = authenticationManager.authenticate(credentials);
+
+        // Se a autenticação foi efetuada com sucesso
+        if (authentication.isAuthenticated()) {
+
+            // Busca os dados do usuário
+            Optional<User> user = userRepository.findByEmail(userLogin.get().getEmail());
+
+            // Se o usuário foi encontrado
+            if (user.isPresent()) {
+
+                // Preenche o Objeto usuarioLogin com os dados encontrados
+                userLogin.get().setId(user.get().getId());
+                userLogin.get().setName(user.get().getName());
+                userLogin.get().setPhoto(user.get().getPhoto());
+                userLogin.get().setToken(generateToken(user.get().getEmail()));
+                userLogin.get().setPassword("");
+
+                // Retorna o Objeto preenchido
+                return userLogin;
+
+            }
+
+        }
+
+        return Optional.empty();
+
+    }
+
+    private String encryptPassword(String password) {
+
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+        return encoder.encode(password);
+
+    }
+
+    private String generateToken(String user) {
+        return "Bearer " + jwtService.generateToken(user);
+    }
+
+}
